0eba0ca9104d650258f13dff85f9a3e8dd84353a
Khomenko <anton.khomenko@gettyimages.com>
2019-10-07 19:10:02 +0300

    modules

--- src/app/products/product-detail.component.ts
+++ src/app/products/product-detail.component.ts
@@ -1,17 +1,38 @@
 import { Component, OnInit } from '@angular/core';
 import { IProduct } from './product';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ProductService } from './product.service';
 
 @Component({
   templateUrl: './product-detail.component.html',
   styleUrls: ['./product-detail.component.css']
 })
 export class ProductDetailComponent implements OnInit {
-  pageTitle: string = 'Product Detail';
-  product: IProduct;
+  pageTitle = 'Product Detail';
+  errorMessage = '';
+  product: IProduct | undefined;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute,
+              private router: Router,
+              private productService: ProductService) { }
 
   ngOnInit() {
+    const param = this.route.snapshot.paramMap.get('id');
+
+    if (param) {
+      const id = +param;
+      this.getProduct(id);
+    }
   }
 
+  getProduct(id: number) {
+    this.productService.getProduct(id).subscribe({
+      next: product => this.product = product,
+      error: err => this.errorMessage = err
+    });
+  }
+
+  onBack(): void {
+    this.router.navigate(['/products']);
+  }
 }
--- src/app/products/product.service.ts
+++ src/app/products/product.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { IProduct } from './product';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
-import { catchError, tap } from 'rxjs/operators';
+import { catchError, tap, map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -11,6 +11,13 @@ export class ProductService {
   private productUrl = 'api/products/products.json';
 
   constructor(private http: HttpClient) {}
+
+  getProduct(id: number): Observable<IProduct | undefined> {
+    return this.getProducts()
+      .pipe(
+        map((products: IProduct[]) => products.find(p => p.productId === id))
+      );
+  }
 
   getProducts(): Observable<IProduct[]> {
     return this.http.get<IProduct[]>(this.productUrl).pipe(
@@ -22,7 +29,7 @@ export class ProductService {
   private handleError(err: HttpErrorResponse) {
     let errorMessage = '';
     if (err.error instanceof ErrorEvent) {
-      errorMessage ='An error occured: ' + err.error.message;
+      errorMessage = 'An error occured: ' + err.error.message;
     } else {
       errorMessage = 'Server returned code: ' + err.status + ', error message is: ' + err.message;
     }
